6d8a76dfa234f531f75a2bc90e974be2b5241237
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2025-08-19 17:11:31 -0700

    it comes in any color you like, so long as it's black

--- src/coffea/dataset_tools/filespec.py
+++ src/coffea/dataset_tools/filespec.py
@@ -96,7 +96,7 @@ class CoffeaFileDict(
                 CoffeaParquetFileSpec,
                 CoffeaUprootFileSpecOptional,
                 CoffeaParquetFileSpecOptional,
-            ]
+            ],
         ]
     ],
     DictMethodsMixin,
