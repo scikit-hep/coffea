21051f002966f97d4f98268d699a165692b9fce4
Andres Rios Tascon <ariostas@gmail.com>
2025-10-14 16:10:13 -0400

    Added simple RNTuple test

---
+++ tests/test_nanoevents_rntuple.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from coffea.nanoevents import BaseSchema, NanoAODSchema, NanoEventsFactory
+
+
+def test_base_schema(tests_directory):
+    path = f"{tests_directory}/samples/nano_dimuon_rntuple.root"
+    factory = NanoEventsFactory.from_root({path: "Events"}, schemaclass=BaseSchema)
+    events = factory.events()
+
+    assert all(f in events.fields for f in ["Electron_pt", "Muon_eta", "Tau_phi"])
+
+    array = events.Electron_pt
+    assert np.isclose(array[0, 0], 17.627574920654297)
+
+
+def test_nanoaod_schema(tests_directory):
+    path = f"{tests_directory}/samples/nano_dimuon_rntuple.root"
+    factory = NanoEventsFactory.from_root({path: "Events"}, schemaclass=NanoAODSchema)
+    events = factory.events()
+
+    assert all(
+        f in events.Electron.fields for f in ["pt", "eta", "phi", "mass", "charge"]
+    )
+    assert all(f in events.Muon.fields for f in ["pt", "eta", "phi", "mass", "charge"])
+    assert all(f in events.Tau.fields for f in ["pt", "eta", "phi", "mass", "charge"])
+    assert all(f in events.Photon.fields for f in ["pt", "eta", "phi"])
+    assert all(f in events.Jet.fields for f in ["pt", "eta", "phi", "mass"])
+
+    assert np.isclose(events.Electron.pt[0, 0], 17.627574920654297)
