21c5b5ca95b4e1633a9cfad9d37bc7635d34809a
Nick Smith <nick.smith@cern.ch>
2025-11-05 12:03:24 -0600

    Catch exceptions

--- src/coffea/_compute.py
+++ src/coffea/_compute.py
@@ -1,11 +1,11 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from itertools import chain, islice, repeat
+from itertools import chain, islice, repeat, starmap
 from queue import Queue, ShutDown
 from threading import Condition, Thread
 import time
-from typing import Callable, Iterator, Literal, Protocol, Any, Sized, TypeVar
+from typing import Callable, Iterator, Literal, Protocol, Sized, TypeVar
 
-import numpy
 
 T = TypeVar("T")
 
@@ -16,14 +16,21 @@ class ResultType(Protocol):
         ...
 
 
-EventsArray = Sized
-"Awkward array of events or similar"
+class EventsArray(Sized, Protocol):
+    "Awkward array of events or similar"
+
+    # metadata: dict[str, Any]
 
 
-class Processor(Protocol):
+EventsFunc = Callable[[EventsArray], ResultType]
+"Function that processes an EventsArray and returns a ResultType"
+
+
+class ProcessorABC(ABC):
+    @abstractmethod
     def process(self, events: EventsArray) -> ResultType:
         """Process a chunk of events and return a result."""
-        ...
+        raise NotImplementedError
 
 
 class Computable(Protocol):
@@ -41,62 +48,70 @@ class Computable(Protocol):
 
 
 @dataclass(slots=True)
-class ProcessStep:
+class Step:
     path: str
-    step: tuple[int, int]
-    processor: Processor
+    entry_range: tuple[int, int]
+
+    def __len__(self) -> int:
+        return self.entry_range[1] - self.entry_range[0]
+
+
+@dataclass(slots=True)
+class StepWorkElement:
+    func: EventsFunc
+    step: Step
 
     def __call__(self) -> ResultType:
-        """Load the events for this step and process them."""
-        events = "A" * (self.step[1] - self.step[0])  # Dummy implementation
-        return self.processor.process(events)
+        # Dummy implementation of event loading
+        info = str(self.step.entry_range)
+        events = info + "A" * (len(self.step) - len(info))  # Dummy events
+        return self.func(events)
+
+
+class StepIterable(ABC):
+    @abstractmethod
+    def iter_steps(self) -> Iterator[Step]:
+        """Return an iterator over steps in the computation."""
+        raise NotImplementedError
+
+    def apply(self, func: EventsFunc | ProcessorABC) -> "StepwiseComputable":
+        if isinstance(func, ProcessorABC):
+            func = func.process
+        return StepwiseComputable(func=func, iterable=self)
 
 
 @dataclass
-class File:
+class StepwiseComputable:
+    func: EventsFunc
+    iterable: StepIterable
+
+    def __iter__(self) -> Iterator[Callable[[], ResultType]]:
+        return map(StepWorkElement, repeat(self.func), self.iterable.iter_steps())
+
+
+@dataclass
+class File(StepIterable):
     path: str
     steps: list[tuple[int, int]]
 
-    def apply(self, processor: Processor) -> "ProcessableFile":
-        return ProcessableFile(file=self, processor=processor)
-
-
-@dataclass(slots=True)
-class ProcessableFile:
-    file: File
-    processor: Processor
-
-    def __iter__(self):
-        return map(
-            ProcessStep, repeat(self.file.path), self.file.steps, repeat(self.processor)
-        )
+    def iter_steps(self) -> Iterator[Step]:
+        return map(Step, repeat(self.path), self.steps)
 
 
 @dataclass
-class Dataset:
+class Dataset(StepIterable):
     files: list[File]
-
-    def apply(self, processor: Processor) -> "ProcessableDataset":
-        return ProcessableDataset(dataset=self, processor=processor)
-
-
-@dataclass(slots=True)
-class ProcessableDataset:
-    dataset: Dataset
-    processor: Processor
-    traversal: Literal["chain", "roundrobin"] = "chain"
+    traversal: Literal["depth", "breadth"] = "depth"
     """The traversal strategy for iterating over files and steps in the dataset.
 
-    "chain" means to process all steps in one file before moving to the next file.
-    "roundrobin" means to process the first step in all files, then the second step in all files, etc.
+    "depth" means to process all steps in one file before moving to the next file.
+    "breadth" means to process the first step in all files, then the second step in all files, etc.
     """
 
-    def __iter__(self):
-        if self.traversal == "roundrobin":
+    def iter_steps(self) -> Iterator[Step]:
+        if self.traversal == "breadth":
             raise NotImplementedError
-        return chain.from_iterable(
-            map(ProcessableFile, self.dataset.files, repeat(self.processor))
-        )
+        return chain.from_iterable(map(File.iter_steps, self.files))
 
 
 class Task(Protocol):
@@ -109,25 +124,23 @@ class Task(Protocol):
 
         The partial result may either be because the task is not yet complete,
         or because the computation failed on a subset of the data.
+
+        TODO: add cancel parameter to indicate whether to stop ongoing computation?
         """
         ...
 
-    def wait(self) -> None: ...
+    def wait(self) -> None:
+        """Block until the computation is complete.
+
+        TODO: add timeout parameter and return_when options?
+        c.f. https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.wait
+        FIRST_COMPLETED, FIRST_EXCEPTION, ALL_COMPLETED
+        """
+        ...
 
     def done(self) -> bool: ...
 
     def cancel(self) -> None: ...
-
-
-class Backend(Protocol):
-    def compute(self, item: Computable) -> Task:
-        """Launch a computation and return a Task representing it.
-
-        The backend holds any resources needed to perform the computation,
-        such as a thread pool, process pool, or cluster connection. It should
-        manage a queue of tasks and execute them in FIFO order.
-        """
-        ...
 
 
 @dataclass
@@ -158,35 +171,86 @@ class EmptyResult:
         return other
 
 
+@dataclass(slots=True)
+class TaskElement:
+    index: int
+    func: Callable[[], ResultType]
+
+    def __call__(self) -> ResultType:
+        return self.func()
+
+
+@dataclass(slots=True)
+class FailedTaskElement(TaskElement):
+    exception: Exception
+    retries: int
+    # TODO: last attempt time
+
+
+ErrorAction = Literal["retry", "continue", "abort"]
+
+
+@dataclass
+class ErrorPolicy:
+    fail_fast: bool = False
+    """If True, abort the computation on the first error encountered."""
+    # TODO: exception-type dependent policy, max retries, backoff
+
+    def first_action(
+        self, element: TaskElement, exception: Exception
+    ) -> tuple[FailedTaskElement, ErrorAction]:
+        """Determine action to take on first failure of a task element."""
+        new_element = FailedTaskElement(
+            index=element.index,
+            func=element.func,
+            exception=exception,
+            retries=0,
+        )
+        if self.fail_fast:
+            return new_element, "abort"
+        return new_element, "continue"
+
+    def retry_action(self, element: FailedTaskElement) -> ErrorAction:
+        """Determine action to take on retry failure of a task element."""
+        if self.fail_fast:
+            return "abort"
+        return "continue"
+
+
 class SimpleTask:
     item: Computable
-    _iter: Iterator[Callable[[], ResultType]]
+    error_policy: ErrorPolicy
+    _iter: Iterator[TaskElement]
     _index: int
+    "Index of the next task element to process, consistent with _output and _failures"
     _output: ResultType
-    _errors: list[tuple[int, Exception]]
+    _failures: list[FailedTaskElement]
     _done: bool
     _cv: Condition
 
     def __init__(self, item: Computable) -> None:
         self.item = item
-        self._iter = iter(item)
+        self.error_policy = ErrorPolicy()
+        self._iter = starmap(TaskElement, enumerate(item))
         self._index = 0
         self._output = EmptyResult()
-        self._errors = []
+        self._failures = []
         self._done = False
         self._cv = Condition()
 
     def result(self) -> ResultType:
         self.wait()
-        if self._errors:
+        if self._failures:
             # Reraise the first error encountered
-            raise self._errors[0][1] from None
+            # TODO: friendly error explaining how to access partial results,
+            # adjust error policy to continue on errors, etc.
+            raise self._failures[0].exception from None
         return self._output
 
     def partial_result(self) -> tuple[ResultType, Computable]:
         with self._cv:
             if not self.done():
-                if self._errors:
+                if self._failures:
                     raise NotImplementedError(
                         "Partial result with errors not implemented"
                     )
@@ -197,12 +261,18 @@ class SimpleTask:
                 return self._output, resumable
         return self._output, RetryableComputation(
             original=self.item,
-            failed_indices=frozenset(idx for idx, _ in self._errors),
+            failed_indices=frozenset(element.index for element in self._failures),
         )
 
     def wait(self) -> None:
         with self._cv:
             self._cv.wait_for(self.done)
+
+    def _set_done(self) -> None:
+        with self._cv:
+            self._done = True
+            # only notification needed, since all waiters are in wait()
+            self._cv.notify_all()
 
     def done(self) -> bool:
         return self._done
@@ -212,15 +282,40 @@ class SimpleTask:
 
     def _run(self) -> None:
         for task_element in self._iter:
-            result = task_element()
-            with self._cv:
-                self._index += 1
-                # This could use a more sophisticated merging strategy
-                self._output = self._output + result
-                self._cv.notify_all()
-        with self._cv:
-            self._done = True
-            self._cv.notify_all()
+            try:
+                result = task_element()
+            except Exception as ex:
+                new_element, action = self.error_policy.first_action(task_element, ex)
+                with self._cv:
+                    if action == "abort":
+                        self._failures.append(new_element)
+                        self._index += 1
+                        return self._set_done()
+                    elif action == "retry":
+                        raise NotImplementedError("Retry action not implemented")
+                    elif action == "continue":
+                        self._failures.append(new_element)
+                        self._index += 1
+                        continue
+            else:
+                with self._cv:
+                    # This could use a more sophisticated merging strategy
+                    self._output = self._output + result
+                    self._index += 1
+        return self._set_done()
+
+
+class Backend(Protocol):
+    def compute(self, item: Computable) -> Task:
+        """Launch a computation and return a Task representing it.
+
+        The backend holds any resources needed to perform the computation,
+        such as a thread pool, process pool, or cluster connection. It should
+        manage a queue of tasks and execute them in FIFO order.
+
+        Probably this should have a context manager interface to clean up resources.
+        """
+        ...
 
 
 def _work(task_queue: Queue[SimpleTask]) -> None:
@@ -257,16 +352,6 @@ class SimpleBackend:
         return task
 
 
-def default_backend() -> Backend:
-    """Get the default computation backend."""
-    return SimpleBackend()
-
-
-def compute(item: Computable, backend: Backend = default_backend()) -> Task:
-    """Compute the given item using the specified backend."""
-    return backend.compute(item)
-
-
 if __name__ == "__main__":
     # Stress test performance of compute with a dummy dataset and processor
     stepsize = 10_000
@@ -285,7 +370,7 @@ if __name__ == "__main__":
         ]
     )
 
-    class SimpleProcessor:
+    class SimpleProcessor(ProcessorABC):
         def process(self, events: EventsArray) -> ResultType:
             return len(events)  # Dummy processing: return number of events
 
@@ -295,8 +380,10 @@ if __name__ == "__main__":
     # but on the other hand, the ProcessableDataset has them separated out easily enough
     computable = dataset.apply(processor)
 
+    backend = SimpleBackend()
+
     tic = time.monotonic()
-    task = compute(computable)
+    task = backend.compute(computable)
     task.wait()
     toc = time.monotonic()
     print(f"Computation took {toc - tic:.2f} seconds")
@@ -304,13 +391,24 @@ if __name__ == "__main__":
     assert task.result() == stepsize * steps_per_file * num_files
 
     # Try a partial result
-    computable = dataset.apply(processor)
-    task = compute(computable)
-    time.sleep(0.4)
+    task = backend.compute(computable)
+    dt = 0.4
+    time.sleep(dt)
     part, resumable = task.partial_result()
-    print(f"Partial result after 1s: {part}")
+    print(f"Partial result after {dt}s: {part}")
     print(f"Resumable computation has {len(list(resumable))} remaining steps")
 
-    resumed_task = compute(resumable)
+    resumed_task = backend.compute(resumable)
     final_result = part + resumed_task.result()
+    print(final_result)
     assert final_result == stepsize * steps_per_file * num_files
+
+    class BuggyProcessor(ProcessorABC):
+        def process(self, events: EventsArray) -> ResultType:
+            if hash(events) % 100 == 0:
+                raise RuntimeError("Simulated processing error")
+            return len(events)
+
+    buggy_computable = dataset.apply(BuggyProcessor())
+    buggy_task = backend.compute(buggy_computable)
+    buggy_task.result()
