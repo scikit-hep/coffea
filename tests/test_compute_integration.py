dce40a97c41ad8cedf46b00fbbe988c4f0e9fdc7
Nick Smith <nick.smith@cern.ch>
2025-11-05 23:35:59 -0600

    profile

--- tests/test_compute_integration.py
+++ tests/test_compute_integration.py
@@ -1,6 +1,5 @@
 import time
 
-
 from coffea.compute import (
     Dataset,
     EventsArray,
@@ -9,6 +8,7 @@ from coffea.compute import (
     ResultType,
 )
 from coffea.compute.backends.threaded import SingleThreadedBackend
+from coffea.compute.errors import ErrorPolicy
 
 
 class DummyProcessor(ProcessorABC):
@@ -41,17 +41,32 @@ def test_threaded_backend_compute():
         ]
     )
 
-    processor = DummyProcessor()
-
-    # Binding the processor to the dataset makes the computable object not easy to serialize
-    # but on the other hand, the ProcessableDataset has them separated out easily enough
-    computable = dataset.apply(processor)
-
     with SingleThreadedBackend() as backend:
+        # Binding the processor to the dataset makes the computable object not easy to serialize
+        # but on the other hand, the ProcessableDataset has them separated out easily enough
+        computable = dataset.apply(DummyProcessor())
         tic = time.monotonic()
         task = backend.compute(computable)
         task.wait()
         toc = time.monotonic()
         print(f"Computation took {toc - tic:.2f} seconds")
-        print(f"Processed {num_files * steps_per_file} steps")
+        print(f"Processed {num_files * steps_per_file} steps with dummy processor")
         assert task.result() == stepsize * steps_per_file * num_files
+
+        computable = dataset.apply(BuggyProcessor())
+        tic = time.monotonic()
+        task = backend.compute(
+            computable, error_policy=ErrorPolicy(continue_on=(ValueError,))
+        )
+        task.wait()
+        toc = time.monotonic()
+        print(f"Computation took {toc - tic:.2f} seconds")
+        print(f"Processed {num_files * steps_per_file} steps with buggy processor")
+        part, resumable = task.partial_result()
+        print(f"Partial result: {part}, resumable length: {len(list(resumable))}")
+
+
+if __name__ == "__main__":
+    # For use with profiling, e.g.
+    # py-spy record -f speedscope tests/test_compute_integration.py
+    test_threaded_backend_compute()
